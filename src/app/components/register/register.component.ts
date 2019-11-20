7e7f5e1994ac7365c67b36c1bd4cb3edf958bf11
Tamirat <tfisseha@fh.org>
2019-11-20 11:03:12 +0300

    incorporate material design and login and register components

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.less']
+})
+export class RegisterComponent implements OnInit {
+
+  Roles: any = ['Admin', 'Author', 'Reader'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
